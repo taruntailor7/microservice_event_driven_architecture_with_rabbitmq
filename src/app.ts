0721d3eb5419dddf06d84c830af6e9120f8caae5
Tarun Tailor <taruntailor7@gmail.com>
2024-05-11 22:56:17 +0530

    products get api created

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
 import * as express from "express";
+import { Request, Response} from "express";
 import * as cors from "cors";
 import { createConnection } from "typeorm";
+import { Product } from "./entity/product";
 
 createConnection().then(db => {
+    const productRepository = db.getRepository(Product);
+
     const app = express();
     
     app.use(cors({
@@ -10,6 +14,11 @@ createConnection().then(db => {
     }))
     
     app.use(express.json());
+
+    app.get('/api/products', async (req: Request, res: Response) => {
+        const products = await productRepository.find();
+        res.json(products);
+    })
     
     console.log("Listening on port: 8000"); 
     app.listen(8000);
