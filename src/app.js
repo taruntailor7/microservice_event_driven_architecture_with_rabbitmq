2b8db12807d3a344428198eea52f8cb1acc0b079
Tarun Tailor <taruntailor7@gmail.com>
2024-05-11 22:26:02 +0530

    app.js code added

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("hello World!");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var express = require("express");
+var cors = require("cors");
+var app = express();
+app.use(cors({
+    origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"]
+}));
+app.use(express.json());
+console.log("Listening on port: 8000");
+app.listen(8000);
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,13 @@
-console.log("hello World!"); 
+import * as express from "express";
+import * as cors from "cors";
+
+const app = express();
+
+app.use(cors({
+    origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"]
+}))
+
+app.use(express.json());
+
+console.log("Listening on port: 8000"); 
+app.listen(8000);
