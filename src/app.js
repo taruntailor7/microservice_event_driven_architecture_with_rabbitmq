b768344ceb5cd4ac0aecb612f23c45a7b6f57401
Tarun Tailor <taruntailor7@gmail.com>
2024-05-11 22:40:54 +0530

    type orm added or mysql connection and also connection established and product table also created

--- src/app.js
+++ src/app.js
@@ -2,10 +2,13 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var express = require("express");
 var cors = require("cors");
-var app = express();
-app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"]
-}));
-app.use(express.json());
-console.log("Listening on port: 8000");
-app.listen(8000);
+var typeorm_1 = require("typeorm");
+(0, typeorm_1.createConnection)().then(function (db) {
+    var app = express();
+    app.use(cors({
+        origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"]
+    }));
+    app.use(express.json());
+    console.log("Listening on port: 8000");
+    app.listen(8000);
+});
--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
 import * as express from "express";
 import * as cors from "cors";
+import { createConnection } from "typeorm";
 
-const app = express();
-
-app.use(cors({
-    origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"]
-}))
-
-app.use(express.json());
-
-console.log("Listening on port: 8000"); 
-app.listen(8000);
+createConnection().then(db => {
+    const app = express();
+    
+    app.use(cors({
+        origin: ["http://localhost:3000", "http://localhost:8080", "http://localhost:4200"]
+    }))
+    
+    app.use(express.json());
+    
+    console.log("Listening on port: 8000"); 
+    app.listen(8000);
+})
